a054e8ee08c955ba225541696cfb89479f98f83f
wanchana <wanchana@wanchana-PC>
2014-12-28 11:20:07 +0700

    commint

--- src/main/java/com/wanchana/cpe4235_home_work/SeparateString_Ans.java
+++ src/main/java/com/wanchana/cpe4235_home_work/SeparateString_Ans.java
@@ -12,10 +12,35 @@ import java.util.Scanner;
  * @author wanchana
  */
 public class SeparateString_Ans {
+    private static boolean is(char ch,char start,char end){
+        return (int)start >= (int)ch && (int)ch <= (int)end;
+    }
+    private static boolean isString(char ch){
+        
+        return is(ch,'A','Z') || is(ch,'a','z');
+    }
+    private static boolean isNumber(char ch){
+        
+        return is(ch,'0','9');
+    }
     public static void main(String[] args) {
-//        Scanner scan = new Scanner(System.in);
-//        String input = scan.next();
-//        System.out.println("Input -> : " + input);
+        Scanner scan = new Scanner(System.in);
+        String input = scan.next();
+        System.out.println("Input -> : " + input);
         System.out.println((int)'A');
+        
+        String str = "";
+        String number = "";
+        for(int i=0;i<input.length();i++){
+            char ch = input.charAt(i);
+            if(isNumber(input.charAt(i))){
+                number = number + input.charAt(i);
+            }
+            if(isString(ch)){
+                str = str + ch;
+            }
+        }
+        System.out.println("Number : " + number);
+        System.out.println("String : " + str);
     }
 }
